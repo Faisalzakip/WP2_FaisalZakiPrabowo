f1416309b31192face896f550e7b9606a68849a5
Faisalzakip <fzakiprabowo@gmail.com>
2022-09-20 12:39:04 +0700

    pertemuan 2

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+use CodeIgniter\Test\CIUnitTestCase;
+
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Faisal Zaki Prabowo
+            Saya tinggal di daerah Tapos
+            Olahraga yang saya sukai adalah Basket";
+    }
+}
